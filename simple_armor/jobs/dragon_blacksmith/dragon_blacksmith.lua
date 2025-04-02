ac0f9822acd06d783c848e13b556be6e964010ed
sdarck2 <135269616+sdarck2@users.noreply.github.com>
2025-04-02 00:17:45 -0300

    Add files via upload

---
+++ simple_armor/jobs/dragon_blacksmith/dragon_blacksmith.lua
@@ -0,0 +1,6 @@
+local CraftingJob = require 'stonehearth.jobs.crafting_job'
+
+local Dragon_BlacksmithClass = class()
+radiant.mixin(Dragon_BlacksmithClass, CraftingJob)
+
+return Dragon_BlacksmithClass
